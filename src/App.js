127a3387be48f1df708d298243e9bc1adc21f908
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-11-12 22:33:37 +0530

    Layouts added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import "./index.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeLayout from "./layouts/HomeLayout/HomeLayout";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomeLayout} />
+      </Switch>
+    </Router>
   );
 }
 
