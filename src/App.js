2dbeb4de710fd0c81634f7fdf7d0e0bfb6ba6da8
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-11-13 02:42:21 +0530

    Login Ui fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeLayout from "./layouts/HomeLayout/HomeLayout";
+import LoginLayout from "./layouts/LoginLayout/LoginLayout";
+import ProfileLayout from "./layouts/ProfileLayout/ProfileLayout";
 
 function App() {
+  const isLoggedIn = false;
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={HomeLayout} />
+        <Route
+          path="/login"
+          exact
+          component={isLoggedIn ? ProfileLayout : LoginLayout}
+        />
+        {/* <Route path="/profile" exact component={ProfileLayout} /> */}
       </Switch>
     </Router>
   );
